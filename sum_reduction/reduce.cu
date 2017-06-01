6dc44403a57642cf953c9dd4585b2669f15635a3
mark-poscablo <mposcablo@gmail.com>
2017-05-31 19:50:22 -0500

    Removed NVIDIA code to move it to another branch

--- sum_reduction/reduce.cu
+++ sum_reduction/reduce.cu
@@ -73,27 +73,6 @@ void block_sum_reduce(unsigned int* const d_block_sums,
 	}
 }
 
-__global__ void reduce0(int* g_idata, int*g_odata) {
-	extern __shared__ int sdata[];
-
-	// each thread loads one element from global to shared mem
-	unsigned int tid = threadIdx.x;
-	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-	sdata[tid] = g_idata[i];
-	__syncthreads();
-
-	// do reduction in shared mem
-	for (unsigned int s = 1; s < blockDim.x; s *= 2) {
-		if (tid % (2 * s) == 0) {
-			sdata[tid] += sdata[tid + s];
-		}
-		__syncthreads();
-	}
-
-	// write result for this block to global mem
-	if (tid == 0) g_odata[blockIdx.x] = sdata[0];
-}
-
 void print_d_array(unsigned int* d_array, unsigned int len)
 {
 	unsigned int* h_array = new unsigned int[len];
